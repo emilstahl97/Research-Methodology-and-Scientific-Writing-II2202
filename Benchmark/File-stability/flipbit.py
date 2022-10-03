db2316d153f403ebf4cef8834b82114226a99530
Emil Ståhl <emil.stahl@icloud.com>
2022-10-03 16:36:29 +0200

    Update flipbit.py

--- Benchmark/File-stability/flipbit.py
+++ Benchmark/File-stability/flipbit.py
@@ -3,9 +3,11 @@ import shutil
 from Bash import Bash
 import pandas as pd
 
+
 class FlipBit(Bash):
     def __init__(self) -> None:
         self.Bash = Bash()
+        self.cwd = os.getcwd()
         
     
     def iterateFiles(self) -> None:
@@ -29,12 +31,15 @@ class FlipBit(Bash):
        
     
     def readTest(self):
-        path = os.getcwd()+"/flippedData/"
+        path = '/Users/emilstahl/Documents/GitHub/Research-Methodology-and-Scientific-Writing-II2202/Benchmark/File-stability/flippedData/'
+        os.chdir(path)
         for file in os.listdir(path):
-             if file.endswith('.csv'):
-                 print(file + " is a csv file")       
-                 pd.read_csv(file)
-    
+            if file.endswith('.csv'):
+                 try:
+                    df = pd.read_csv(file, engine='python')
+                    print("File is readable")
+                 except:
+                        print("File is not readable")
 
 
 if __name__ == "__main__":
